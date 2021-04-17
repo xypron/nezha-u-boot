922dbccace49382d6aedcce18e5d494b444ce0b8
Samuel Holland <samuel@sholland.org>
2021-04-17 13:33:54 -0500

    sunxi: Enable support for SCP firmware on H3
    Now that issues with the BROM have been sorted out, we can implement
    PSCI system suspend on H3 by delegating to SCP firmware. Let's start by
    including the firmware in the FIT image and starting the coprocessor if
    valid firmware is loaded.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/configs/sun8i.h
+++ include/configs/sun8i.h
@@ -12,6 +12,9 @@
 					 CONFIG_ARMV7_SECURE_MAX_SIZE)
 #define SUNXI_RESUME_SIZE		1024
 
+#define SUNXI_SCP_BASE			(SUNXI_RESUME_BASE + SUNXI_RESUME_SIZE)
+#define SUNXI_SCP_MAX_SIZE		(16 * 1024)
+
 #include <configs/sunxi-common.h>
 
 #endif /* __CONFIG_H */
