27bfe62477a1515fa31793879ac80a4d3e42e5b2
Samuel Holland <samuel@sholland.org>
2021-08-08 11:05:23 -0500

    clk: sunxi: Add support for D1 CCU
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/dt-bindings/clock/sun50i-rtc.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _DT_BINDINGS_CLK_SUN50I_RTC_CCU_H_
+#define _DT_BINDINGS_CLK_SUN50I_RTC_CCU_H_
+
+#define CLK_OSC32K		0
+#define CLK_OSC32K_FANOUT	1
+#define CLK_IOSC		2
+
+#define CLK_RTC_SPI		8
+
+#endif /* _DT_BINDINGS_CLK_SUN50I_RTC_CCU_H_ */
