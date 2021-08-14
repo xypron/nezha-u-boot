4ca9aacfa47184db649fae4d4b99b6d426544ce9
Samuel Holland <samuel@sholland.org>
2021-08-14 12:34:58 -0500

    sunxi: Remove sunxi-specific name_to_gpio macro
    This clarifies which callers must be updated to complete the DM_GPIO
    conversion.
    
    The only caller of name_to_gpio in generic code is inside the !DM_GPIO
    block in cmd/gpio.c. DM_GPIO is always selected on sunxi, so that code
    cannot be reached. And after this commit, there are only two remaining
    implementations of name_to_gpio.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- arch/arm/include/asm/arch-sunxi/gpio.h
+++ arch/arm/include/asm/arch-sunxi/gpio.h
@@ -244,7 +244,6 @@ int sunxi_gpio_set_drv(u32 pin, u32 val);
 int sunxi_gpio_set_pull(u32 pin, u32 val);
 int sunxi_name_to_gpio_bank(const char *name);
 int sunxi_name_to_gpio(const char *name);
-#define name_to_gpio(name) sunxi_name_to_gpio(name)
 
 #if !defined CONFIG_SPL_BUILD && defined CONFIG_AXP_GPIO
 int axp_gpio_init(void);
