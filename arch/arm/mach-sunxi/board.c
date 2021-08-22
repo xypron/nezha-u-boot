75d379cde1268df4c1cc5b0b4ec1381217c5fe52
Samuel Holland <samuel@sholland.org>
2021-08-22 15:43:41 -0500

    sunxi: Avoid duplicate reset_cpu with SYSRESET enabled
    The sysreset uclass unconditionally provides a definition of the
    reset_cpu() function. So does the sunxi board code. Omit our definition
    when SYSRESET is enabled. This allows the build to succeed, even though
    sysreset may not yet be functional due to a lack of back-end drivers.
    
    Series-to: sunxi
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- arch/arm/mach-sunxi/board.c
+++ arch/arm/mach-sunxi/board.c
@@ -347,6 +347,7 @@ void board_init_f(ulong dummy)
 }
 #endif
 
+#if !CONFIG_IS_ENABLED(SYSRESET)
 void reset_cpu(void)
 {
 #if defined(CONFIG_SUNXI_GEN_SUN4I) || defined(CONFIG_MACH_SUN8I_R40)
@@ -377,6 +378,7 @@ void reset_cpu(void)
 	while (1) { }
 #endif
 }
+#endif
 
 #if !CONFIG_IS_ENABLED(SYS_DCACHE_OFF) && !defined(CONFIG_ARM64)
 void enable_caches(void)
