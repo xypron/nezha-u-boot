7ab7b53d86e5a752468943ec7979c9a5c39e4251
Samuel Holland <samuel@sholland.org>
2021-08-28 13:13:52 -0500

    pinctrl: sunxi: Add sunxi GMAC pinmuxes
    Pin lists and mux values were taken from the Linux drivers.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/pinctrl/sunxi/pinctrl-sun7i-a20.c
+++ drivers/pinctrl/sunxi/pinctrl-sun7i-a20.c
@@ -8,6 +8,7 @@
 
 static const struct sunxi_pinctrl_function sun7i_a20_pinctrl_functions[] = {
 	{ "emac",	2 },	/* PA0-PA17 */
+	{ "gmac",	5 },	/* PA0-PA17 */
 	{ "gpio_in",	0 },
 	{ "gpio_out",	1 },
 #if IS_ENABLED(CONFIG_UART0_PORT_F)
--- drivers/pinctrl/sunxi/pinctrl-sun9i-a80.c
+++ drivers/pinctrl/sunxi/pinctrl-sun9i-a80.c
@@ -7,6 +7,7 @@
 #include "pinctrl-sunxi.h"
 
 static const struct sunxi_pinctrl_function sun9i_a80_pinctrl_functions[] = {
+	{ "gmac",	2 },	/* PA0-PA17 */
 	{ "gpio_in",	0 },
 	{ "gpio_out",	1 },
 #if IS_ENABLED(CONFIG_UART0_PORT_F)
