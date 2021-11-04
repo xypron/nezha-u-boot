9b244f69f8e181ca44fe7c5bbf539642ba479a9e
Samuel Holland <samuel@sholland.org>
2021-11-04 17:49:15 -0500

    pinctrl: sunxi: Hack up the driver for the D1
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/pinctrl/sunxi/pinctrl-sunxi.c
+++ drivers/pinctrl/sunxi/pinctrl-sunxi.c
@@ -9,6 +9,7 @@
 #include <malloc.h>
 
 #include <asm/gpio.h>
+#include "../../../arch/arm/include/asm/arch-sunxi/gpio.h"
 
 extern U_BOOT_DRIVER(gpio_sunxi);
 
@@ -49,7 +50,7 @@ static const char *sunxi_pinctrl_get_pin_name(struct udevice *dev,
 					      uint pin_selector)
 {
 	const struct sunxi_pinctrl_desc *desc = dev_get_priv(dev);
-	static char pin_name[sizeof("PN31")];
+	static char pin_name[sizeof("PN31")] __section(".data");
 
 	snprintf(pin_name, sizeof(pin_name), "P%c%d",
 		 pin_selector / SUNXI_GPIOS_PER_BANK + desc->first_bank + 'A',
